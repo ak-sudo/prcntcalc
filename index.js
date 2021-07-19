b97eb5d1eab193f4f1225a7189ca54b9202368ff
ak-sudo <72704561+ak-sudo@users.noreply.github.com>
2021-07-19 17:15:03 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+function calc(){
+    var first, second, third, fourth, fifth, sixth, result;
+    first = Number(document.getElementById("first").value);
+    second = Number(document.getElementById("second").value);
+    third = Number(document.getElementById("third").value);
+    fourth = Number(document.getElementById("fourth").value);
+    fifth = Number(document.getElementById("fifth").value);
+    sixth = Number(document.getElementById("sixth").value);
+    result = (first+second+third+fourth+fifth+sixth)*100/120;
+    if(result >= 80){
+        document.getElementById("msg").style.color="green";
+    }
+    if(result < 80){
+        document.getElementById("msg").style.color="rgba(255, 238, 5, 0.952)";
+    }
+    if(result <= 30){
+        document.getElementById("msg").style.color="red";
+    }
+    write = document.getElementById("msg");
+    write.innerText = "You scored "+ result+ "%";
+}
