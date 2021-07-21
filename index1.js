7c3857b66a7786b133e11d0c0d2b3be5a46a1712
ak-sudo <72704561+ak-sudo@users.noreply.github.com>
2021-07-21 13:49:40 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -2,7 +2,7 @@ function redirector(){
     yourClass = Number(document.getElementById("class").value);
 
     if (yourClass = [1,2,3,4,5,6,7,8,9,10,11,12]){
-        window.location.href = "index.html";
+        window.location.href = "/prcnt/calc.html";
     }
     else{
         alert("You have entered an invalid class. Please Try Again!");
