0d0ac3127ad97475f7fa66703bcc97a07939a120
ak-sudo <72704561+ak-sudo@users.noreply.github.com>
2021-07-21 14:06:24 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,7 +1,7 @@
 function redirector(){
     yourClass = Number(document.getElementById("class").value);
 
-    if (yourClass = [1,2,3,4,5,6,7,8,9,10,11,12]){
+    if (yourClass = 1){
         window.location.href = "/prcntcalc/calc.html";
     }
     else{
