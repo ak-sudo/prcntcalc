f93cba4726fb7e09aed20ef33f664803c513126b
ak-sudo <72704561+ak-sudo@users.noreply.github.com>
2021-07-21 13:19:03 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,10 @@
+function redirector(){
+    yourClass = Number(document.getElementById("class").value);
+
+    if (yourClass = [1,2,3,4,5,6,7,8,9,10,11,12]){
+        window.location.href = "index.html";
+    }
+    else{
+        alert("You have entered an invalid class. Please Try Again!");
+    }
+}
