0077fb8be396d0347ffa67ad967e936802b99804
ak-sudo <72704561+ak-sudo@users.noreply.github.com>
2021-07-21 17:12:21 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,13 +1,19 @@
 function redirector(){
     yourClass = Number(document.getElementById("class").value);
-    if (yourClass < 6){
-        window.location.href = "/prcntcalc/c1to5.html"
-    }
-    if (yourClass >= 6){
-        window.location.href = "/prcntcalc/calc.html";
-    }
-    if (yourClass >= 13){
-        alert("You have entered an invalid class. Please Try Again!");
+    
+    if (yourClass == 0){
+        alert("Inavlid Class!");
         return false;
     }
+    if (yourClass >= 13){
+        alert("You have entered an invalid class.");
+        return false;
+    }
+    if (yourClass < 6){
+        window.location.href = "https://ak-sudo.github.io/prcntcalc/c1to5.html";
+    }
+    if (yourClass >= 6){
+        window.location.href = "https://ak-sudo.github.io/prcntcalc/calc.html";
+    }
+
 }
