f16ab17fcb31b775c38bdd0fad0e228a21ec3ecb
ak-sudo <72704561+ak-sudo@users.noreply.github.com>
2021-07-21 14:11:45 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,10 +1,10 @@
 function redirector(){
     yourClass = Number(document.getElementById("class").value);
-
-    if (yourClass = 1){
-        window.location.href = "/prcntcalc/calc.html";
-    }
-    else{
+    
+    if (yourClass >= 13){
         alert("You have entered an invalid class. Please Try Again!");
+    }
+    if (yourClass >= 1){
+        window.location.href = "/prcntcalc/calc.html";
     }
 }
